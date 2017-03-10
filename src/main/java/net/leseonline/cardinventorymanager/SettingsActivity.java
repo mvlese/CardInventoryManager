e52e7d90a2dc45ffa72be878de801b717849b8ff
mlese <lesemv@gmail.com>
2017-03-09 22:24:07 -0500

    Added drag to TextView.

--- src/main/java/net/leseonline/cardinventorymanager/SettingsActivity.java
+++ src/main/java/net/leseonline/cardinventorymanager/SettingsActivity.java
@@ -1,13 +1,26 @@
 package net.leseonline.cardinventorymanager;
 
+import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+    private TextView mPlayerTextView;
+    private TextView mTeamTextView;
+    private TextView mValueTextView;
+    private TextView mYearTextView;
+    private TextView mConditionTextView;
+    private TextView mCompanyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,143 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mPlayerTextView = (TextView)findViewById(R.id.sort_player_text_view);
+        mPlayerTextView.setTag("player-tag");
+        mPlayerTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handleOnLongClickForTextView(v);
+                return true;
+            }
+        });
+
+        mTeamTextView = (TextView)findViewById(R.id.sort_team_text_view);
+        mTeamTextView.setTag("team-tag");
+        mTeamTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handleOnLongClickForTextView(v);
+                return true;
+            }
+        });
+
+        mValueTextView = (TextView)findViewById(R.id.sort_value_text_view);
+        mValueTextView.setTag("value-tag");
+        mValueTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handleOnLongClickForTextView(v);
+                return true;
+            }
+        });
+
+        mYearTextView = (TextView)findViewById(R.id.sort_year_text_view);
+        mYearTextView.setTag("year-tag");
+        mYearTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handleOnLongClickForTextView(v);
+                return true;
+            }
+        });
+
+        mConditionTextView = (TextView)findViewById(R.id.sort_condition_text_view);
+        mConditionTextView.setTag("condition-tag");
+        mConditionTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handleOnLongClickForTextView(v);
+                return true;
+            }
+        });
+
+        mCompanyTextView = (TextView)findViewById(R.id.sort_company_text_view);
+        mCompanyTextView.setTag("company-tag");
+        mCompanyTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                handleOnLongClickForTextView(v);
+                return true;
+            }
+        });
+
+    }
+
+    private void handleOnLongClickForTextView(View v) {
+        // Create a new ClipData.
+        // This is done in two steps to provide clarity. The convenience method
+        // ClipData.newPlainText() can create a plain text ClipData in one step.
+
+        // Create a new ClipData.Item from the ImageView object's tag
+        ClipData.Item item = new ClipData.Item((String)v.getTag());
+
+        // Create a new ClipData using the tag as a label, the plain text MIME type, and
+        // the already-created item. This will create a new ClipDescription object within the
+        // ClipData, and set its MIME type entry to "text/plain"
+        v.getTag();
+        ClipData dragData = ClipData.newPlainText((String)v.getTag(), "Player");
+
+        // Instantiates the drag shadow builder.
+        View.DragShadowBuilder myShadow = new MyDragShadowBuilder(mPlayerTextView);
+
+        // Starts the drag
+
+        v.startDrag(dragData,  // the data to be dragged
+                myShadow,  // the drag shadow builder
+                null,      // no need to use local data
+                0          // flags (not currently used, set to 0)
+        );
+    }
+
+    private static class MyDragShadowBuilder extends View.DragShadowBuilder {
+
+        // The drag shadow image, defined as a drawable thing
+        private static Drawable shadow;
+
+        // Defines the constructor for myDragShadowBuilder
+        public MyDragShadowBuilder(View v) {
+
+            // Stores the View parameter passed to myDragShadowBuilder.
+            super(v);
+
+            // Creates a draggable image that will fill the Canvas provided by the system.
+            shadow = new ColorDrawable(Color.GRAY);
+        }
+
+        // Defines a callback that sends the drag shadow dimensions and touch point back to the
+        // system.
+        @Override
+        public void onProvideShadowMetrics (Point size, Point touch) {
+            // Defines local variables
+            int width, height;
+
+            // Sets the width of the shadow to half the width of the original View
+            width = getView().getWidth();
+
+            // Sets the height of the shadow to half the height of the original View
+            height = getView().getHeight();
+
+            // The drag shadow is a ColorDrawable. This sets its dimensions to be the same as the
+            // Canvas that the system will provide. As a result, the drag shadow will fill the
+            // Canvas.
+            shadow.setBounds(0, 0, width, height);
+
+            // Sets the size parameter's width and height values. These get back to the system
+            // through the size parameter.
+            size.set(width, height);
+
+            // Sets the touch point's position to be in the middle of the drag shadow
+            touch.set(width / 2, height / 2);
+        }
+
+        // Defines a callback that draws the drag shadow in a Canvas that the system constructs
+        // from the dimensions passed in onProvideShadowMetrics().
+        @Override
+        public void onDrawShadow(Canvas canvas) {
+
+            // Draws the ColorDrawable in the Canvas passed in from the system.
+            shadow.draw(canvas);
+        }
     }
 
 }
